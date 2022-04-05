a3c540bb6e67daebcf27cd3cb38ad9f57397fef7
arief ramadhan <arief.ramadhan43@gmail.com>
2022-04-05 17:13:59 +0700

    Initial Commit

---
+++ Collection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collection
+//
+//  Created by Arief Ramadhan on 05/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
